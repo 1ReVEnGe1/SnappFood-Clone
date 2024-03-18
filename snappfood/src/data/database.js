69dd5f3a24d68e33f46e0969fa72cb98e870ed9c
Amirreza <96297073+1ReVEnGe1@users.noreply.github.com>
2024-03-19 00:45:49 +0330

    creating database
    adding main categories

---
+++ snappfood/src/data/database.js
@@ -0,0 +1,77 @@
+import asianFoods from '../assets/website_image_asian_1.jpg'
+import burgers from '../assets/website_image_burger_1.jpg'
+import fastfoods from '../assets/website_image_fastfood_1.jpg'
+import guilaniFoods from '../assets/website_image_gilani_1.jpg'
+import iraniFoods from '../assets/website_image_irani_1.jpg'
+import italyFoods from '../assets/website_image_italy_1.jpg'
+import kebabs from '../assets/website_image_kebab_1.jpg'
+import pizzas from '../assets/website_image_pizza_1.jpg'
+import salads from '../assets/website_image_salad_1.jpg'
+import sandwiches from '../assets/website_image_sandwich_1.jpg'
+import seafoods from '../assets/website_image_seafood_1.jpg'
+import nuggets from '../assets/website_image_sokhari_1.jpg'
+
+export const CATEGORIESs = [, "فست فود", "کباب", "پیتزا", "برگر", "ساندویچ", "سوخاری", "پاستا", "سالاد", "دریایی", "بین الملل", "گیلانی"];
+export const CATEGORIES =[
+    {
+        id:1,
+        categoryName: "ایرانی",
+        image:iraniFoods
+    },
+    {
+        id:2,
+        categoryName: "فست فود",
+        image:fastfoods
+    },
+    {
+        id:3,
+        categoryName: "کباب",
+        image:kebabs
+    },
+    {
+        id:4,
+        categoryName: "پیتزا",
+        image:pizzas
+    },
+    {
+        id:5,
+        categoryName: "برگر",
+        image:burgers
+    },
+    {
+        id:6,
+        categoryName: "ساندویچ",
+        image:sandwiches
+    },
+    {
+        id:7,
+        categoryName: "سوخاری",
+        image:nuggets
+    },
+    {
+        id:8,
+        categoryName: "پاستا",
+        image:italyFoods
+    },
+    {
+        id:9,
+        categoryName: "سالاد",
+        image:salads
+    },
+    {
+        id:10,
+        categoryName: "دریایی",
+        image:seafoods
+    },
+    {
+        id:11,
+        categoryName: "بین الملل",
+        image:asianFoods
+    },
+    {
+        id:12,
+        categoryName: "گیلانی",
+        image:guilaniFoods
+    },
+]
+
